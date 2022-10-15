5f19a44e4b4e43cfb1fe003d3b103b39e1115eaf
doston701 <abduvohidovdoston0305@gmail.com>
2022-10-15 08:44:11 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let btnfirst1 = document.querySelector(".btn1");
+let btnfirst2 = document.querySelector(".btn2");
+let btnfirst3 = document.querySelector(".btn3");
+
+btnfirst1.addEventListener("click", function () {
+  alert("HTML");
+  console.log(prompt("What is HTML?"));
+  console.log(prompt("Why do we open index.html?"));
+  console.log(prompt("How many tag is avaliable in html?"));
+  console.log(prompt("What means meta?"));
+});
+
+btnfirst2.addEventListener("click", function () {
+  alert("SCSS");
+  console.log(prompt("What is SCSS?"));
+  console.log(prompt("Why do we need SCSS"));
+  console.log(prompt("What is the adventages of SCSS"));
+  console.log(prompt("what is disadventages of SCSS"));
+});
+
+btnfirst3.addEventListener("click", function () {
+  alert("JAVA SCRIPT");
+  console.log(prompt("what is JAVA SCRIPT"));
+  console.log(prompt("what do we need JAVA SCRIPT "));
+  console.log(prompt("what can we do with JAVA SCRIPT"));
+  console.log(prompt("Write something about JAVA SCRIPT"));
+});
